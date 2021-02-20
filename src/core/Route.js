12eed5fd21184e57a8a36323ca14e832fbc787ea
GHOST <ghostdevbusiness@gmail.com>
2021-02-20 19:31:11 +0000

    feat: route class

---
+++ src/core/Route.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = class Route {
+    constructor({ fullname }) {
+        const sanitisedPath = path
+            .normalize(fullname)
+            .replace(/\\/g, '/')
+            .replace(/(\.[^\n.]*)$/gm, '');
+
+        const route = sanitisedPath
+            .match(/(?=routes)([^\n])+/g)[0]
+            .slice(6)
+            .replace(/index/g, '');
+
+        this.path = path.normalize(fullname).replace(/\\/g, '/');
+        this.route =
+            route.endsWith('/') && route.length > 1
+                ? route.slice(0, -1)
+                : route;
+        this.directoryRoute = !!this.path.match(/(?:(index)(\.(\w)*)?)$/gm);
+    }
+};
