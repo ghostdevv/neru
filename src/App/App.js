19ba5d2c3d5e66f8f1fde9ececa51367b757c423
GHOST <ghostdevbusiness@gmail.com>
2021-02-21 19:41:27 +0000

    feat: middlewear support via .use

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,7 @@
 module.exports = class App {
+    #wares = [];
+
+    // HTTP Methods
     #connect = [];
     #delete = [];
     #get = [];
@@ -25,39 +28,62 @@ module.exports = class App {
         };
     }
 
+    use(ware) {
+        this.#wares.push(ware);
+    }
+
+    // Used to validate middlewear and merge middlewear added with app.use
+    updateMethod(current, next) {
+        if (!(Array.isArray(current) || Array.isArray(next)))
+            throw new TypeError('updateMethod expects two arrays');
+
+        if (!next.every((x) => typeof x == 'function'))
+            throw new TypeError('All middlewear provided should be a function');
+
+        // Add all currently registerd middlewear
+        next = [...this.#wares, ...next];
+
+        // Update the current route method stack
+        current.push(next);
+
+        return current;
+    }
+
+    // HTTP Methods
+
     connect(...wares) {
-        this.#connect.push(wares);
+        this.#connect = this.updateMethod(this.#connect, wares);
     }
 
     delete(...wares) {
-        this.#delete.push(wares);
+        this.#delete = this.updateMethod(this.#get, wares);
     }
 
     get(...wares) {
-        this.#get.push(wares);
+        this.#get = this.updateMethod(this.#get, wares);
     }
 
     head(...wares) {
-        this.#head.push(wares);
+        this.#head = this.updateMethod(this.#head, wares);
     }
 
     options(...wares) {
-        this.#options.push(wares);
+        this.#options = this.updateMethod(this.#options, wares);
     }
 
     patch(...wares) {
-        this.#patch.push(wares);
+        this.#patch = this.updateMethod(this.#patch, wares);
     }
 
     post(...wares) {
-        this.#post.push(wares);
+        this.#post = this.updateMethod(this.#post, wares);
     }
 
     put(...wares) {
-        this.#put.push(wares);
+        this.#put = this.updateMethod(this.#put, wares);
     }
 
     trace(...wares) {
-        this.#trace.push(wares);
+        this.#trace = this.updateMethod(this.#trace, wares);
     }
 };
