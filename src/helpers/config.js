b0fbb0ed264b43d63bc2f4b4b6769550abd0f682
GHOST <ghostdevbusiness@gmail.com>
2021-04-30 20:46:36 +0100

    feat: switch to esm

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,7 +1,7 @@
-const { configent } = require('configent');
-const defaults = require('../../config.defaults');
+import { configent } from 'configent';
+import defaults from '../config.defaults.js';
 
-module.exports = (inp) =>
+export default (inp) =>
     configent(defaults, inp, {
         name: 'neru',
     });
