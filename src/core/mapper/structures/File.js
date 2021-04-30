367d778aae099177f248db7a08540f5e0c53e1db
GHOST <ghostdevbusiness@gmail.com>
2021-05-01 00:37:19 +0100

    feat: File class

---
+++ src/core/mapper/structures/File.js
@@ -0,0 +1,14 @@
+import { clean } from '../index.js';
+import { parse } from 'path';
+
+export default class File {
+    constructor(path) {
+        const { dir, base, ext, name } = parse(path);
+
+        this.path = clean(path);
+        this.dir = clean(dir);
+        this.base = clean(base);
+        this.ext = clean(ext);
+        this.name = clean(name);
+    }
+}
