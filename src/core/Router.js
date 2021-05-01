226cba8bed08ac97a490dd83186b5679e9413057
GHOST <ghostdevbusiness@gmail.com>
2021-05-01 01:21:47 +0100

    exe: route loader

--- src/core/Router.js
+++ src/core/Router.js
@@ -1,11 +1,29 @@
 export default class Router {
     #server;
-    #routes;
     #options;
+
+    #routesLoaded = false;
 
     constructor(server, routes, options) {
         this.#server = server;
-        this.#routes = routes;
         this.#options = options;
+
+        if (!(routes instanceof Map))
+            throw new TypeError(`Expected a map for routes`);
+
+        this.loadRoutes(routes);
+    }
+
+    async loadRoutes(routes) {
+        if (this.#routesLoaded) throw new Error('Routes already loaded');
+        this.#routesLoaded = true;
+
+        for (const route of routes.values()) {
+            try {
+                const data = await import(route.fileUrl);
+                if (!data || !data.default)
+                    throw new Error(`Unable to load route: ${route.route}`);
+            } catch (error) {}
+        }
     }
 }
