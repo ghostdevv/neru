6dd75c954512179d8c48dcb1fdaecfa9579da7ed
GHOST <ghostdevbusiness@gmail.com>
2021-05-02 12:33:13 +0100

    fix: improve route resolving

--- src/core/mapper/RouteFile.js
+++ src/core/mapper/RouteFile.js
@@ -7,16 +7,22 @@ export default class RouteFile {
         this.fileUrl = file.url;
         this.routesDir = routesDir;
 
-        this.route = RouteFile.cleanRoute(
-            relative(routesDir, file.path).slice(0, -file.ext.length),
-        );
-
+        this.route = RouteFile.resolveRoute(file, routesDir);
         this.expressPath = RouteFile.parseExpressPath(this.route);
     }
 
     static cleanRoute(route) {
         route = clean(route);
         return route.startsWith('/') ? route : `/${route}`;
+    }
+
+    static resolveRoute(file, routesDir) {
+        const route = file.path
+            .match(/(src\/routes\/)([^\n])+/gi)[0]
+            .replace(new RegExp(routesDir, 'gim'), '')
+            .slice(0, -file.ext.length);
+
+        return route.endsWith('index') ? route.slice(0, -5) : route;
     }
 
     static parseExpressPath(route) {
