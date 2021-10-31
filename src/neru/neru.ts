758683c86d896df263066937e982296cc49d8697
GHOSt <ghostdevbusiness@gmail.com>
2021-10-31 22:01:19 +0000

    feat: routes

--- src/neru/neru.ts
+++ src/neru/neru.ts
@@ -1,9 +1,11 @@
+import { resolveRoutes } from './routes/resolve';
 import { createLayer } from '../adapters/layer';
 import { createLogger } from '../utils/logger';
 import { validateOptions } from './options';
+import { castToArray } from 'ghoststools';
 
+import type { NeruOptions, NeruParams } from './options';
 import type { Adapter } from '../adapters/adapter';
-import type { NeruParams } from './options';
 
 export const neru = <AdapterType extends Adapter>({
     adapter: inpAdapter,
@@ -12,6 +14,13 @@ export const neru = <AdapterType extends Adapter>({
 }: NeruParams<AdapterType>) => {
     const logger = createLogger(inpOptions.debug);
 
-    const options = validateOptions(inpOptions, logger);
+    const options = validateOptions(inpOptions as NeruOptions, logger);
     const layer = createLayer(inpAdapter, logger);
+
+    const routes = [];
+
+    for (const routesDir of castToArray<string>(options.routes))
+        routes.push(...resolveRoutes(routesDir));
+
+    console.log(routes);
 };
