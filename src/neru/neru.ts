0b9f70896dcfdac03bc14c07a72120d77192a7fe
GHOSt <ghostdevbusiness@gmail.com>
2021-10-31 15:10:58 +0000

    feat: logs errors

--- src/neru/neru.ts
+++ src/neru/neru.ts
@@ -1,15 +1,24 @@
 import { validateAdapter } from '../adapters/adapter';
+import type { Adapter } from '../adapters/adapter';
+import { createLogger } from '../utils/logger';
 import type { NeruOptions } from './options';
+import { coloured } from '../utils/colour';
 
-export const neru = <AdapterType>({
+export const neru = <AdapterType extends Adapter>({
     adapter,
     server,
     ...options
 }: NeruOptions<AdapterType>) => {
+    const logger = createLogger(options.debug);
+
+    const adapterLogger = logger.createChild(
+        coloured(`[ADAPTER: ${adapter.name || 'unknown'}]`, 35),
+    );
+
     const { error } = validateAdapter(adapter);
 
     if (error) {
-        console.error(error.annotate());
+        adapterLogger.error(error.annotate());
 
         throw new Error(
             'Invalid adapter, please contact the author with the above error',
