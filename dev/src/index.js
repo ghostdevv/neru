e5ac7a0969a382c5d3713c4ba1b91e12f2f61729
GHOSt <ghostdevbusiness@gmail.com>
2021-11-08 21:58:05 +0000

    dev: update

--- dev/src/index.js
+++ dev/src/index.js
@@ -1,20 +1,19 @@
+import { adapter } from '@nerujs/hapi';
+import Hapi from '@hapi/hapi';
 import { neru } from 'neru';
 
-/**
- * @type {import('neru').Adapter<string, number>}
- */
-const adapter = {
-    name: 'testing-adapter',
+const server = Hapi.server({
+    host: 'localhost',
+    port: 35309,
+});
 
-    formatParamRoute: (slug) => `{${slug}}`,
-    formatSpreadRoute: (slug) => `{${slug}**}`,
-
-    addRoute: (server, route) => console.log({ route }),
-};
-
-neru({
+await neru({
     adapter,
-    server: '',
+    server,
     routes: 'src/routes',
     debug: true,
 });
+
+await server.start();
+
+console.log(`Online on ${server.info.uri}`);
