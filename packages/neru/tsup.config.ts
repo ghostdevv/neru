1b3bb9fcf8fdc35fc4ea0080b1d848a970c17acd
GHOSt <ghostdevbusiness@gmail.com>
2021-11-08 22:07:49 +0000

    chore: update tsup config

--- packages/neru/tsup.config.ts
+++ packages/neru/tsup.config.ts
@@ -3,7 +3,7 @@ import type { Options } from 'tsup';
 export const tsup: Options = {
     splitting: false,
     sourcemap: false,
-    clean: true,
+    clean: false,
     dts: true,
     keepNames: true,
     target: 'esnext',
