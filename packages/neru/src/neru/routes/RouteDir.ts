9e4c26903df983d754ac72bc93aeb362faf1ffc1
GHOSt <ghostdevbusiness@gmail.com>
2021-11-14 18:40:48 +0000

    fix: validation guard

--- packages/neru/src/neru/routes/RouteDir.ts
+++ packages/neru/src/neru/routes/RouteDir.ts
@@ -5,10 +5,10 @@ import { existsSync } from 'fs';
 export class RouteDir {
     public readonly path: string;
 
-    constructor(rawPath: string) {
+    constructor(rawPath: string, validate = true) {
         this.path = stripTrailingSlash(posixify(resolve(normalize(rawPath))));
 
-        if (!existsSync(this.path))
+        if (!existsSync(this.path) && validate)
             throw new Error(`Unable to find routes directory ${this.path}`);
     }
 }
