e21c2257c279279039ca3c874a4fff3fed23363d
GHOSt <ghostdevbusiness@gmail.com>
2021-11-22 17:36:47 +0000

    fix: typing for user inputted route

--- packages/adapter-hapi/src/index.ts
+++ packages/adapter-hapi/src/index.ts
@@ -1,28 +1,31 @@
 import type { ServerRoute, Server } from '@hapi/hapi';
 import type { Adapter } from 'neru';
 
+type NeruHapiServerRoute = Omit<ServerRoute, 'path' | 'method'>;
+
 export const adapter: Adapter<Server, ServerRoute> = {
     name: 'hapi',
 
     formatParamRoute: (slug) => `{${slug}}`,
     formatSpreadRoute: (slug) => `{${slug}*}`,
 
-    addRoute: (server, route, methods) => {
-        const methodsArray: [string, ServerRoute][] = Object.entries(methods);
+    addRoute: (server, { route }, methods) => {
+        const methodsArray: [string, NeruHapiServerRoute][] =
+            Object.entries(methods);
 
         for (const [method, data] of methodsArray) {
             if (typeof data != 'object')
                 throw new TypeError(
-                    `Expected route data for route "${route.route}" with method "${method}" - try the route fn`,
+                    `Expected route data for route "${route}" with method "${method}" - try the route fn`,
                 );
 
             server.route({
                 ...data,
                 method,
-                path: route.route,
+                path: route,
             });
         }
     },
 };
 
-export const route = (route: ServerRoute) => route;
+export const route = (route: NeruHapiServerRoute) => route;
