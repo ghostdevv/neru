4230a30ddc3530651617f306b992015338028226
GHOSt <ghostdevbusiness@gmail.com>
2021-11-26 17:16:41 +0000

    fix: don't strip trailing /

--- packages/neru/src/neru/routes/RouteDir.ts
+++ packages/neru/src/neru/routes/RouteDir.ts
@@ -6,7 +6,7 @@ export class RouteDir {
     public readonly path: string;
 
     constructor(rawPath: string, validate = true) {
-        this.path = stripTrailingSlash(posixify(resolve(normalize(rawPath))));
+        this.path = posixify(resolve(normalize(rawPath)));
 
         if (!existsSync(this.path) && validate)
             throw new Error(`Unable to find routes directory ${this.path}`);
