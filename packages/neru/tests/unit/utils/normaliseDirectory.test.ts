466789fba2f6830d070c97cfeb03ad2a1d6daf16
GHOST <ghostdevbusiness@gmail.com>
2022-03-05 00:28:14 +0000

    fix: test

--- packages/neru/tests/unit/utils/normaliseDirectory.test.ts
+++ packages/neru/tests/unit/utils/normaliseDirectory.test.ts
@@ -3,12 +3,15 @@ import assert from 'assert';
 import { test } from 'uvu';
 import { sep } from 'path';
 
-import { normaliseDirectory } from '../../../src/utils/fs';
+import { normaliseDirectory, normaliseSlashes } from '../../../src/utils/fs';
 
 test('correctly normalise slash depending on the platform', () => {
     const dir = normaliseDirectory('/a\\b/c');
 
-    assert.equal(dir, resolve(sep == '\\' ? '/a/b/c' : '/a\\b/c'));
+    assert.equal(
+        dir,
+        normaliseSlashes(resolve(sep == '\\' ? '/a/b/c' : '/a\\b/c')),
+    );
 });
 
 test.run();
