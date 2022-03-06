573586e97b94291aebd38ac2d397613a21563f4c
GHOST <ghostdevbusiness@gmail.com>
2022-03-06 00:05:30 +0000

    chore: remove unused imports

--- packages/neru/src/utils/fs.ts
+++ packages/neru/src/utils/fs.ts
@@ -1,5 +1,5 @@
 import { readdirRecursive, posixify } from 'ghoststools';
-import { normalize, resolve, sep } from 'path';
+import { sep } from 'path';
 
 export const readFiles = (files: string) =>
     readdirRecursive(files, { filter: (f) => !f.startsWith('_') });
