30f72d46de213b4b4bfba8b4c45306aabed6e1e7
GHOSt <ghostdevbusiness@gmail.com>
2022-03-06 13:41:42 +0000

    fix: test for system sep

--- packages/neru/tests/unit/utils/readFiles.test.ts
+++ packages/neru/tests/unit/utils/readFiles.test.ts
@@ -1,4 +1,4 @@
-import { join, dirname, normalize } from 'path';
+import { join, dirname, normalize, sep } from 'path';
 import { fileURLToPath } from 'url';
 import assert from 'assert';
 import { test } from 'uvu';
@@ -9,25 +9,21 @@ const __dirname = normalize(dirname(fileURLToPath(import.meta.url)));
 const testfiles = join(__dirname, './testfiles');
 const rawFiles = readFiles(testfiles);
 
-const files = new Set(
-    rawFiles.map((f) => f.replace(__dirname, '').replace(/\\/g, '/')),
-);
+const files = new Set(rawFiles.map((f) => f.replace(__dirname, '')));
+
+/**
+ * * We do this so that we can check rawFiles returns paths with system correct separator
+ */
+const $ = (path: string) => path.replace(/\$/g, sep);
 
 test('files are correctly read', async () => {
-    assert.ok(
-        files.has('/testfiles/index.js'),
-        "Doesn't have '/testfiles/index.js'",
-    );
+    const p1 = $('$testfiles$index.js');
+    const p2 = $('$testfiles$nested$more$hello.js');
+    const p3 = $('$testfiles$nested$more$world.js');
 
-    assert.ok(
-        files.has('/testfiles/nested/more/hello.js'),
-        "Doesn't have '/testfiles/nested/more/hello.js'",
-    );
-
-    assert.ok(
-        files.has('/testfiles/nested/more/world.js'),
-        "Doesn't have '/testfiles/nested/more/world.js'",
-    );
+    assert.ok(files.has(p1), `Doesn't have ${p1}`);
+    assert.ok(files.has(p2), `Doesn't have ${p2}`);
+    assert.ok(files.has(p3), `Doesn't have ${p3}`);
 });
 
 test.run();
