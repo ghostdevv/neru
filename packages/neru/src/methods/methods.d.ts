5682cb05241853e2d0c3ea0d578fe127e932ffa8
GHOSt <ghostdevbusiness@gmail.com>
2022-03-20 12:36:35 +0000

    fix: remove all prop

--- packages/neru/src/methods/methods.d.ts
+++ packages/neru/src/methods/methods.d.ts
@@ -1,12 +1,7 @@
 import type { LowercaseMethod } from '@nerujs/methods';
 
 export interface RouteMethods<MethodValue = unknown>
-    extends Record<LowercaseMethod, MethodValue | MethodValue[]> {
-    /**
-     * This allows you to respond to any method
-     */
-    all?: MethodValue;
-}
+    extends Record<LowercaseMethod, MethodValue | MethodValue[]> {}
 
 export interface RawRouteMethods<MethodValue = unknown>
     extends Omit<RouteMethods<MethodValue>, 'delete'> {
