810a181118a44d55e32867b3b52c881b6399f7d2
GHOSt <ghostdevbusiness@gmail.com>
2022-04-13 21:00:14 +0100

    refactor: style

--- packages/neru/src/routes/Route.ts
+++ packages/neru/src/routes/Route.ts
@@ -2,44 +2,48 @@ import { filePathToRoute, formatRoutePath, formatRoutePathSegment } from './util
 import type { RouteMethods } from '../methods/methods.d';
 import { Adapter } from '../adapters/adapter';
 
+export interface RouteParams<AdapterType extends Adapter, MethodType> {
+    filePath: string;
+    routesDirectory: string;
+
+    base: string;
+
+    adapter: AdapterType;
+    methods: Partial<RouteMethods<MethodType>>;
+}
+
 export class Route<AdapterType extends Adapter, MethodType> {
     /**
      * The route string
      */
     public readonly route: string;
 
-    /**
-     * The file this route belongs to
-     */
-    public readonly filePath: string;
-
-    /**
-     * The directory this route was found in
-     */
-    public readonly routesDirectory: string;
-
-    /**
-     * The adapter the route is using
-     */
-    public readonly adapter: AdapterType;
-
-    /**
-     * All methods exported from the file
-     */
-    public readonly methods: Partial<RouteMethods<MethodType>>;
-
     constructor(
-        filePath: string,
-        base: string,
-        routesDirectory: string,
-        adapter: AdapterType,
-        methods: Partial<RouteMethods<MethodType>>,
-    ) {
-        this.routesDirectory = routesDirectory;
-        this.filePath = filePath;
-        this.adapter = adapter;
-        this.methods = methods;
+        /**
+         * The file this route belongs to
+         */
+        public readonly filePath: string,
 
+        /**
+         * The base path for the route
+         */
+        public readonly base: string,
+
+        /**
+         * The directory this route was found in
+         */
+        public readonly routesDirectory: string,
+
+        /**
+         * The adapter the route is using
+         */
+        public readonly adapter: AdapterType,
+
+        /**
+         * All methods exported from the file
+         */
+        public readonly methods: Partial<RouteMethods<MethodType>>,
+    ) {
         // Resolve the route
         this.route = filePathToRoute(filePath, routesDirectory);
         this.route = formatRoutePath(this.route, adapter);
